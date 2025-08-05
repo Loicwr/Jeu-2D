d59249d748733e86b1b2004a568006b43503c8fd
nicolas <lafuentenicolas1@gmail.com>
2025-08-05 17:25:27 +0200

    Add initial project files, including .gitignore, Plastic SCM configurations, and PlayerMovement script with jump functionality

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,23 +2,51 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     public float moveSpeed;
+    public float jumpForce;
+
+
+    private bool isJumping;
+    private bool isGrounded;
+
+
+    public Transform groundCheckLeft;
+    public Transform groundCheckRight;
+
+
     public Rigidbody2D rb;
     private Vector3 velocity = Vector3.zero;
-   
-    
+
+
+    // Calculer vitesse de mouvement
     void FixedUpdate()
     {
+        // il crée une boite de collision entre les deux éléments, si sa entre en contacte avec quelque chose renvoie true 
+        isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
+
+        // Calculer vitesse de mouvement horizontal
         float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        float verticalMovement = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
+        // Est ce que ya une demande de saut
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            isJumping = true;
+        }
+
+        // Effectuer le mouvement
         MovePlayer(horizontalMovement);
-    }
 
+    }
     void MovePlayer(float _horizontalMovement)
     {
-      Vector3  TargetVelocity = new Vector2(_horizontalMovement, rb.linearVelocity.y);
-        rb.linearVelocity = Vector3.SmoothDamp(rb.linearVelocity, TargetVelocity, ref velocity, .05f);
+        // Calculer vélocité de notre cible ( personnage vers le prochain mouvement)
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.linearVelocity.y);
+        rb.linearVelocity = Vector3.SmoothDamp(rb.linearVelocity, targetVelocity, ref velocity, 0.05f);
+
+        if (isJumping == true)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            isJumping = false;
+        }
     }
 }
