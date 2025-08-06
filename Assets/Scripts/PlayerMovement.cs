c5f66cee45faa138f3963b1713bc487a9faac46c
nicolas <lafuentenicolas1@gmail.com>
2025-08-06 19:06:14 +0200

    Refactor PlayerMovement to use a single ground check and improve movement logic; update TagManager and add NoFriction physics material

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,14 +10,18 @@ public class PlayerMovement : MonoBehaviour
     private bool isGrounded;
 
 
-    public Transform groundCheckLeft;
-    public Transform groundCheckRight;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask collisionLayers;
+
 
 
     public Rigidbody2D rb;
     public Animator animator;
     public SpriteRenderer spriteRenderer;
+
     private Vector3 velocity = Vector3.zero;
+    private float horizontalMovement;
 
     void Update()
     {
@@ -26,29 +30,26 @@ public class PlayerMovement : MonoBehaviour
         {
             isJumping = true;
         }
-    }
-
-        // Calculer vitesse de mouvement
-        void FixedUpdate()
-         {
-        // il crée une boite de collision entre les deux éléments, si sa entre en contacte avec quelque chose renvoie true 
-        isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
-
-        // Calculer vitesse de mouvement horizontal
-        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-
-       
-
-        // Effectuer le mouvement
-        MovePlayer(horizontalMovement);
 
         Flip(rb.linearVelocity.x);
 
         // envoie la vitesse horizontal
         float characterVelocity = Mathf.Abs(rb.linearVelocity.x);
         animator.SetFloat("Speed", rb.linearVelocity.x);
+    }
 
+       
+        void FixedUpdate()
+         {
+        // il crée une boite de collision
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, collisionLayers);
+
+        // Calculer vitesse de mouvement horizontal
+        horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        // Effectuer le mouvement
+        MovePlayer(horizontalMovement);
           }
+
     void MovePlayer(float _horizontalMovement)
     {
         // Calculer vélocité de notre cible ( personnage vers le prochain mouvement)
@@ -72,5 +73,11 @@ public class PlayerMovement : MonoBehaviour
         {
             spriteRenderer.flipX = true;
         }
+    }
+    private void OnDrawGizmos()
+    {
+        // Dessine le cercle de vérification du sol
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
