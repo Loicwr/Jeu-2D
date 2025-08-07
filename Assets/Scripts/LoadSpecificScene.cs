2c2343209b9f9b0f8bc18b859f6c7be13a3250df
Nicolas Lafuente <lafuentenicolas1@gmail.com>
2025-08-07 11:52:37 +0200

    Add scene loading functionality and implement DontDestroyOnLoad for specified objects
    - Modified LoadSpecificScene to allow dynamic scene loading based on a public string variable.
    - Introduced a new script, DontDestroyOnLoadScene, to prevent specified GameObjects from being destroyed when loading new scenes.

--- Assets/Scripts/LoadSpecificScene.cs
+++ Assets/Scripts/LoadSpecificScene.cs
@@ -3,11 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class LoadSpecificScene : MonoBehaviour
 {
+
+    public string sceneName;
+
    private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
         {
-            SceneManager.LoadScene("Scene2");
+            SceneManager.LoadScene(sceneName);
         }
     }
 }
