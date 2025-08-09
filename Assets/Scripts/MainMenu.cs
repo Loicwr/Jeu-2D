80e9637c74e18d3cfda2232e85e4d029f011bc72
nicolas <lafuentenicolas1@gmail.com>
2025-08-09 18:44:45 +0200

    add scripts for the main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public string levelToLoad;
+    public GameObject settingsWindow;
+
+    public void StartGame()
     {
-        
+        // Charge la première scène du jeu
+        SceneManager.LoadScene(levelToLoad);
+    }
+    public void SettingsButton()
+    {
+       settingsWindow.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CloseSettings()
     {
-        
+        // Ferme la fenêtre des paramètres
+        settingsWindow.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        // Quitte l'application
+        Application.Quit();
     }
 }
--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
@@ -13,18 +14,25 @@ public class SettingsMenu : MonoBehaviour
 
     public void Start()
     {
-        resolutions = Screen.resolutions;
-        resolutionDropDown.ClearOptions();
+        resolutions = Screen.resolutions.Select(resolutions => new Resolution { width = resolutions.width,height = resolutions.height}).Distinct().ToArray();
 
         List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
 
         for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
+
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height) 
+            {
+                currentResolutionIndex = i;
+            }
         }
 
         resolutionDropDown.AddOptions(options);
+        resolutionDropDown.value = currentResolutionIndex;
+        resolutionDropDown.RefreshShownValue();
     }
 
     public void SetVolume(float volume)
@@ -37,5 +45,12 @@ public class SettingsMenu : MonoBehaviour
     {
         // Met à jour le mode plein écran
         Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        // Met à jour la résolution du jeu
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
